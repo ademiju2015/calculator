8ebf155710715c20b67713fd24e60fc85c2c5a1e
ademiju2015 <ademijuadekanmi44@gmail.com>
2025-06-14 11:51:30 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const CalculatorApp(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
